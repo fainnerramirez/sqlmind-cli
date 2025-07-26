4368ae9f62cd230b50a794d87e8c119717730229
fainnerramirez <fainnerramirez@gmail.com>
2025-07-26 17:26:33 -0500

    feat: se implementa pks inquirir

--- app.ts
+++ app.ts
@@ -1,18 +1,26 @@
-#!/usr/bin/env node
-
 import figlet from "figlet";
 import cli from "inquirer";
 
 console.log(figlet.textSync(`SQLMIND${"   "}CLI`));
 console.log("Tu agente de AI para SQL");
-console.log("Convierte fácilmente lenguaje natural en consultas SQL precisas con un solo clic\n");
+console.log("Convierte fácilmente lenguaje natural en consultas SQL precisas con un solo prompt\n");
 
 const getInputUser = async () => {
     try {
         const input = await cli.prompt({
             type: "input",
             name: "promptSQL",
-            message: "¿Solo escribe lo que necesitas, y yo lo convertiré a SQL?"
+            message: "¿Solo escribe lo que necesitas, y yo lo convertiré a SQL?",
+            validate: (value: string) => {
+                let length: number = value.length;
+                if (length < 5) {
+                    return "❌ tu consulta debe tener un mínimo de 20 caracterés";
+                }
+                if (length > 150) {
+                    return "❌ tu consulta debe tener máximo 150 caractéres";
+                }
+                return true;
+            }
         });
         console.log("Consulta del usuario: ", input.promptSQL);
     } catch (error: any) {
